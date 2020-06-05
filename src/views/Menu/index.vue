ee88c2ee44718b7ae368ce620b49c171c5878a18
dogsteven <caigicungduoc23@gmail.com>
2020-06-06 01:16:15 +0700

    add heroku

--- src/views/Menu/index.vue
+++ src/views/Menu/index.vue
@@ -1,23 +1,15 @@
 <template>
   <v-container>
-    <v-list
-      v-for="(item, key) in $store.state.foods"
-      :key="key"
-    >
+    <v-list>
       <v-list-item
-        @click="() => {}"
+        v-for="(item, key) in $store.state.foods"
+        :key="key"
+        :to="'/item-detail/' + key"
       >
-        <v-list-item-avatar>
-          <v-icon>fas fa-shopping-cart</v-icon>
-        </v-list-item-avatar>
-        <v-list-item-title
-          class="menu-item-title"
-        >
+        <v-list-item-title>
           {{ item.name }}
-          <v-list-item-subtitle
-            class="menu-item-subtitle"
-          >
-            {{ item.price }} VND
+          <v-list-item-subtitle>
+            {{ item.price }}VND
           </v-list-item-subtitle>
         </v-list-item-title>
       </v-list-item>
@@ -36,11 +28,5 @@ export default {
 </script>
 
 <style lang="sass">
-@import "https://fonts.googleapis.com/css?family=Open%20Sans"
 
-.menu-item-title
-  font-size: 30px
-
-.menu-item-subtitle
-  font-family: "Open Sans"
 </style>
