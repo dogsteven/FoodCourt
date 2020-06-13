8b5095fb6d9ff68886ab6e43222978b0346a7bd4
dogsteven <caigicungduoc23@gmail.com>
2020-06-13 21:36:02 +0700

    hehe

--- src/http.js
+++ src/http.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default {
   configuration: {
-    managementURL: 'http://localhost:5000'
+    managementURL: 'http://localhost'
   },
 
   get(route, config = null) {
