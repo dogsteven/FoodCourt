b85fd262ee9de66e6eeac4ad0b2e9c9c2f05633a
Nguyen Tan Dat <dat.nguyen_bk@hcmut.edu.vn>
2020-06-14 20:30:24 +0700

    UPDATED: switch to chip-group

--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -1,10 +1,27 @@
 <template>
   <v-container align="center">
     <v-row justify="space-around">
-      <v-switch v-model="cate1" class="ma-2" label="Món chính"></v-switch>
-      <v-switch v-model="cate2" class="ma-2" label="Món ăn nhanh"></v-switch>
-      <v-switch v-model="cate3" class="ma-2" label="Món ăn sáng"></v-switch>
-      <v-switch v-model="cate3" class="ma-2" label="Món tráng miệng"></v-switch>
+        <v-card
+    class="mx-auto"
+    max-width="1200"
+  > 
+
+    <v-card-text>
+      <h2 class="title mb-2">Chọn loại món ăn </h2>
+
+      <v-chip-group
+        v-model="amenities"
+        column
+        multiple
+      >
+        <v-chip filter outlined>Món chính</v-chip>
+        <v-chip filter outlined>Món tráng miệng</v-chip>
+        <v-chip filter outlined>Món ăn sánge</v-chip>
+        <v-chip filter outlined>Món ăn nhanhk</v-chip>
+        <v-chip filter outlined>Đồ uống</v-chip>
+      </v-chip-group>
+    </v-card-text> 
+  </v-card> 
       <v-container>
         <v-list :dense="dense" :three-line="threeLine" :nav="nav" :avatar="avatar" :rounded="rounded">
         <v-card>
@@ -15,18 +32,22 @@
             <v-list-item v-for="(item, i) in items" :key="i" :inactive="inactive">
               <v-col>
                 <v-row>
-                  <v-list-item-avatar v-if="avatar">
-                    <v-img :src="item.picture"></v-img>
-                  </v-list-item-avatar>    
+                  <v-col>
+                  <v-avatar>
+                  <v-img :src="item.picture" ></v-img>
+                  </v-avatar>
+                  </v-col>
                   <v-list-item-content>
-                    <v-list-item-title v-html="item.name"></v-list-item-title>
+                    <v-title v-html="item.name"></v-title>
                     <v-list-item-subtitle v-html="item.description"></v-list-item-subtitle>
-                    <v-list-item-subtitle v-html="item.price"> VND</v-list-item-subtitle>
                   </v-list-item-content>
+                  <v-col class="text-right">
+                    <v-header v-html="item.price" ></v-header>
+                  </v-col>
                 </v-row>
               </v-col>
               <v-col class="text-right">
-                  <v-btn flat color="orange">Add to cart</v-btn>
+                  <v-btn v-on:click= "addToCart" flat color="orange">Add to cart</v-btn>
               </v-col>
             </v-list-item>
           </v-list-item-group>
@@ -51,17 +72,17 @@ export default {
           name: "Bò kho bánh mì",
           picture:
             "http://images1.baoninhthuan.com.vn/CMSImage/Resources/Uploaded/tbtdaduyet/bo-kho-banh-mi-2014-12-05-18-54.jpg",
-          price: "20000",
+          price: "20000 VND",
           quantity: 0,
           vendorID: 0
         },
         {
           categories: ["món chính", "món ăn sáng"],
-          description: "Đây là món hủ tiếu gia truyền được chế biến từ cám heo",
+          description: "Đây là món hủ tiếu gia truyền",
           name: "Hủ tiếu",
           picture:
             "https://media.cooky.vn/recipe/g4/36783/s800x500/cooky-recipe-cover-r36783.jpg",
-          price: "22000",
+          price: "22000 VND",
           quantity: 0,
           vendorID: 0
         },
@@ -71,7 +92,7 @@ export default {
           name: "Hamburger",
           picture:
             "https://www.simplyhappyfoodie.com/wp-content/uploads/2018/04/instant-pot-hamburgers-3.jpg",
-          price: "200000",
+          price: "200000 VND",
           quantity: 0,
           vendorID: 0
         },
@@ -81,7 +102,7 @@ export default {
           name: "Mì trộn",
           picture:
             "https://vncooking.com/files/cuisine/2019/01/25/mi-tron-muoi-ot-cay-nong-erc6.jpg",
-          price: "15000",
+          price: "15000 VND",
           quantity: 0,
           vendorID: 0
         },
@@ -91,7 +112,7 @@ export default {
           name: "Bánh giò",
           picture:
             "https://beptruong.edu.vn/wp-content/uploads/2016/01/mon-banh-gio.jpg",
-          price: "11000",
+          price: "11000 VND",
           quantity: 0,
           vendorID: 0
         },
@@ -101,7 +122,7 @@ export default {
           name: "Bánh trung thu",
           picture:
             "https://cdn.eva.vn/upload/3-2019/images/2019-08-14/moi-lam-banh-trung-thu-hien-dai-2-nam-da-dep-the-nay-8x-con-mo-duoc-lop-day-hoc-39020892_869523606574753_4848377822114217984_n-1565755347-875-width960height720.jpg",
-          price: "60000",
+          price: "60000 VND",
           quantity: 0,
           vendorID: 0
         },
@@ -111,7 +132,7 @@ export default {
           name: "Cơm gà xối mỡ",
           picture:
             "https://cdn.huongnghiepaau.com/wp-content/uploads/2018/07/com-ga-xoi-mo.jpg",
-          price: "25000",
+          price: "25000 VND",
           quantity: 0,
           vendorID: 0
         },
@@ -121,7 +142,7 @@ export default {
           name: "Phở bò",
           picture:
             "https://media.cooky.vn/recipe/g1/4573/s480x480/recipe4573-prepare-step3-636422898648914257.jpg",
-          price: "22000",
+          price: "22000 VND",
           quantity: 0,
           vendorID: 0
         },
@@ -131,7 +152,7 @@ export default {
           name: "Cà ri bò",
           picture:
             "https://media.cooky.vn/recipe/g3/25296/s480x480/recipe25296-prepare-step5-636486908431470841.jpg",
-          price: "25000",
+          price: "25000 VND",
           quantity: 0,
           vendorID: 0
         },
@@ -141,7 +162,7 @@ export default {
           name: "Cơm tấm",
           picture:
             "http://image.vinanet.vn/zoom/640/Uploaded/huonglan/2019_06_26/26061_EQTV.jpg",
-          price: "25000",
+          price: "25000 VND",
           quantity: 0,
           vendorID: 0
         },
@@ -151,7 +172,7 @@ export default {
           name: "Mì quảng",
           picture:
             "https://khomay3a.com/userfiles/image/mon-ngon-moi-ngay/cach-nau-nuoc-leo-mi-quang-001.jpg",
-          price: "20000",
+          price: "20000 VND",
           quantity: 0,
           vendorID: 0
         }
@@ -171,7 +192,7 @@ export default {
     counter : 0
   }),
   methods: {
-    add: function () {
+    addToCart: function () {
       this.counter += 1
     }
   }
