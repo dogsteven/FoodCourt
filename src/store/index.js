a9208594b71684b29618e01f102aecbe4bd5165b
Nguyen Tan Dat <dat.nguyen_bk@hcmut.edu.vn>
2020-06-20 23:40:55 +0700

    TODO: change to old ver of menu and new filter feature

--- src/store/index.js
+++ src/store/index.js
@@ -4,31 +4,59 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: () => ({
-    isSignedIn: false,
-    foods: {}
-  }),
+  state: {
+    foods: [],
+    carts: [],
+    customer: {
+      id: null,
+      info: {
+        username: null,
+        firstname: null,
+        lastname: null,
+        email: null
+      }
+    }
+  },
   mutations: {
-    setIsSignedIn(state, value) {
-      state.isSignedIn = value
-    },
-    setFoods(state, value) {
-      state.foods = value
-    },
     pushFoodItem(state, value) {
       state.foods.push(value)
+    },
+
+    pushCartItem(state, cart) {
+      state.carts.push(cart)
+    },
+
+    removeItemFromCart(state, index) {
+      state.carts.splice(index, 1)
+    },
+
+    increaseCartItemQuantity(state, { index, amount }) {
+      if (amount <= 0)
+        return
+      state.carts[index].quantity += amount
+    },
+
+    decreaseCartItemQuantity(state, { index, amount }) {
+      if (amount <= 0)
+        return
+      if (state.carts[index].quantity > 1)
+        state.carts[index].quantity = state.carts[index].quantity - amount
+    },
+
+    popCartItem(state, position) {
+      state.carts.splice(position, 1)
+    },
+
+    setCustomer(state, { id, info }) {
+      state.customer.id = id
+      state.customer.info.username = info.username
+      state.customer.info.firstname = info.firstname
+      state.customer.info.lastname = info.lastname
+      state.customer.info.email = info.email
     }
   },
   actions: {
-    queryFoods({ commit }, route) {
-      http.get(route).then((response) => {
-        commit('setFoods', response.data)
-      }).catch((error) => {
-        alert(error)
-      })
-    }
   },
   modules: {
-
   }
 })
