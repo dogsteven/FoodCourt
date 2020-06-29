b9b810769c9344232feb3e8cc17b60d189d70033
Nguyen Huynh Long <long.nguyenhuynh611@gmail.com>
2020-06-29 12:50:20 +0700

    bug sign in

--- src/views/sign-in/index.vue
+++ src/views/sign-in/index.vue
@@ -4,21 +4,21 @@
       <v-img max-height="200" src="../../assets/sign-in-wallpaper.png"></v-img>
 
       <v-card-title>
-        <v-tabs color="brown" fixed-tabs v-model="tab">
+        <v-tabs color="brown" fixed-tabs>
           <v-tab>Sign In</v-tab>
         </v-tabs>
       </v-card-title>
 
-      <v-card-text class="pa-5">
-        <v-text-field color="brown" v-model="username" label="Username"></v-text-field>
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-text-field color="brown" v-model="username" label="Username" :rules="usernameRules"></v-text-field>
         <v-text-field
           color="brown"
           v-model="password"
           label="Password"
           type="password"
-          :rules="[requireSixCharacters]"
+          :rules="passwordRules"
         ></v-text-field>
-      </v-card-text>
+      </v-form>
 
       <v-card-actions>
         <v-btn block color="brown" text @click="SignIn">Sign in</v-btn>
@@ -34,44 +34,35 @@
 import http from "../../http";
 export default {
   methods: {
-    requireSixCharacters(value) {
-      if (this.tab === 0) return true;
-      return value.length >= 6 || value.length === 0;
-    },
-    isEqualToPassword(value) {
-      return value === this.password || value.length === 0;
-    },
     SignIn() {
-      let username = this.username;
-      let password = this.password;
+      if (this.$refs.form.validate()) {
+        let username = this.username;
+        let password = this.password;
 
-      http.server
-        .get("/customer/" + username + "/" + password)
-        .then(response => {
-          let data = response.data;
-          if (data !== null) {
-            this.$store.commit("setCustomer", data);
-            this.$store.commit("setIsSignedIn", true);
-            this.$router.replace({ path: "/menu" });
-          } else this.$store.commit("setIsSignedIn", false);
-        });
-      localStorage.setItem("account", "ok");
-      this.$store.commit("setIsSignedIn", true);
-      this.$router.replace({ path: "/menu" });
+        http.server
+          .get("/customer/" + username + "/" + password)
+          .then(response => {
+            let data = response.data;
+            if (data !== null) {
+              this.$store.commit("setIsSignedIn", true);
+              this.$router.replace({ path: "/menu" });
+            } else {
+              alert("Username or Password is incorrect");
+            }
+          });
+      }
     },
     SignUp() {
       this.$router.replace({ path: "/sign-up" });
     }
   },
   data: () => ({
+    valid: true,
     username: "",
     password: "",
-    repassword: "",
-    firstname: "",
-    lastname: "",
-    email: "",
-    tab: 0,
-    isSignInFailed: false
+    isSignedIn: false,
+    usernameRules: [v => !!v || "Username is required"],
+    passwordRules: [v => !!v || "Password is required"]
   })
 };
 </script>
