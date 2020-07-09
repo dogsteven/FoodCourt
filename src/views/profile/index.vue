b37b5ebcc4d23a8d9e8750c220d99872959eaaa2
Nguyen Huynh Long <long.nguyenhuynh611@gmail.com>
2020-07-09 16:57:12 +0700

    bug profile

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,63 +1,52 @@
 <template>
   <v-container>
-    <v-card
-      max-width="700"
-      class="mx-auto mb-2"
-    >
-      <v-card-title>
-        {{ username }}'s profile
-      </v-card-title>
+    <v-card max-width="700" class="mx-auto mb-2">
+      <v-card-title>{{ username }}'s profile</v-card-title>
       <v-card-text>
-        <v-text-field
-          v-model="password"
-          color="orange"
-          label="Password"
-          type="password"
-        >
-        </v-text-field>
-        <v-text-field
-          v-model="firstname"
-          color="orange"
-          label="First name"
-        >
-        </v-text-field>
-        <v-text-field
-          v-model="lastname"
-          color="orange"
-          label="Last name"
-        >
-        </v-text-field>
-        <v-text-field
-          v-model="email"
-          color="orange"
-          label="Email"
-        >
-        </v-text-field>
+        <v-text-field v-model="password" color="orange" label="Password" type="password"></v-text-field>
+        <v-text-field v-model="firstname" color="orange" label="First name"></v-text-field>
+        <v-text-field v-model="lastname" color="orange" label="Last name"></v-text-field>
+        <v-text-field v-model="email" color="orange" label="Email"></v-text-field>
 
-        <v-btn
-          block
-          text
-          color="green"
-        >
-          Save
-        </v-btn>
+        <v-btn block text color="green" @click="updateProfile()">Save</v-btn>
       </v-card-text>
     </v-card>
-
-    
   </v-container>
 </template>
 
 <script>
+import http from '../../http'
 export default {
   created() {
-    this.username = this.$store.state.customer.username
-    this.password = this.$store.state.customer.password
-    this.firstname = this.$store.state.customer.firstname
-    this.lastname = this.$store.state.customer.lastname
-    this.email = this.$store.state.customer.email
+    this.username = this.$store.state.customer.username;
+    this.password = this.$store.state.customer.password;
+    this.firstname = this.$store.state.customer.firstname;
+    this.lastname = this.$store.state.customer.lastname;
+    this.email = this.$store.state.customer.email;
   },
-
+   methods: {
+    updateProfile() {
+      var dataBody = {
+        password: this.password,
+        firstname: this.firstname,
+        lastname: this.lastname,
+        email: this.email
+      };
+      let config = {
+        "Content-Type": "application/json"
+      };
+      http.server.put("/customer/" + this.$store.state.customer.username + "/" + this.$store.state.customer.password, dataBody, config).then(response => {
+        let data = response.data;
+        console.log(data)
+        if (data !== null) {
+          dataBody.id = data.id;
+          dataBody.registrationTokens = [];
+          localStorage.setItem("customer", JSON.stringify(dataBody));
+          this.$store.commit("setCustomer", dataBody);
+        }
+      });
+    }
+  },
   data: () => ({
     username: "",
     password: "",
@@ -66,9 +55,8 @@ export default {
     email: "",
     isEditable: false
   })
-}
+};
 </script>
 
 <style>
-
 </style>
